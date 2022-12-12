2bcbdd10ae08e1fc24967a983183a9db9a11ff96
Aditya Suresh <ethanaditya@gmail.com>
2022-12-12 08:15:13 -0700

    added boilerplate

---
+++ GridEntity2D.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+class_name GridEntity2D
+
+# Declare member variables here. Examples:
+export(int) var x = 0
+export(int) var y = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
