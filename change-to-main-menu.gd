ff21a64ce7e5ae648f0eec85c3b3efab0e08d8ff
aerits <stev_nm@protonmail.com>
2023-01-25 07:43:11 -0700

    bruh

--- change-to-main-menu.gd
+++ change-to-main-menu.gd
@@ -1,12 +1,4 @@
 extends Node
 
-var simultaneous_scene = preload("res://main menu or something/gaming.tscn").instance()
-
-func _add_a_scene_manually():
-	# This is like autoloading the scene, only
-	# it happens after already loading the main scene.
-	get_tree().get_root().add_child(simultaneous_scene)
-
-
 func _on_Button_pressed():
-	_add_a_scene_manually() # Replace with function body.
+	get_tree().change_scene("res://main menu or something/gaming.tscn")
