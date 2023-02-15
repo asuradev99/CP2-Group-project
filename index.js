05bc59c696864c90f3808ed7ee464b09b494537c
Alongalili0 <64511941+Alongalili0@users.noreply.github.com>
2023-02-15 08:35:32 -0700

    wasd

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-
+const inc = 10;
+let x, y;
 function setup() {
     createCanvas(window.innerWidth,window.innerHeight);
 }
-
+function KeyboardControl(){
+    if (key == 'w') {
+        y -= inc;
+      } else if (key == 'a') {
+        x -= inc;
+      } else if (key == 's') {
+        y += inc;
+      } else if (key == 'd') {
+        x += inc;
+      }
+}
 function draw() {
     background(200);
-    ellipse(window.innerWidth/2,window.innerHeight/2,200,200);
+    ellipse(window.innerWidth/2,window.innerHeight/2,x,y);
+   // rect(40, 120, 120, 40);
 }
 console.log("Hello World");
