c9e13808609c128c6415b617383533504bf5252d
aerits <stev_nm@protonmail.com>
2023-03-03 08:47:19 -0700

    sadf

--- public/player.js
+++ public/player.js
@@ -1,11 +1,11 @@
 class player{
     let x;
     let y;  
-let speed;
-let smoothSpeed = 1;
-let scl = 25.0;
-const count = 3;
-let iToTheta;
+    let speed;
+    let smoothSpeed = 1;
+    let scl = 25.0;
+    const count = 3;
+    let iToTheta;
     
     constructor(playername) {
         
