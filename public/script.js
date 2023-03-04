d7fdbab73667d4bbec02cdbbc70921a635ca1c6f
aerits <stev_nm@protonmail.com>
2023-03-04 08:51:37 -0700

    added some comments

--- public/script.js
+++ public/script.js
@@ -10,6 +10,7 @@ const socket = io({
 //const socket = io('http://game.0000727.xyz:8080')
 var clientid
 
+// ---------------------- create a variable containing the id of the client
 socket.on("connect", () => {
   clientid = socket.id
   console.log('connected')
@@ -17,6 +18,8 @@ socket.on("connect", () => {
 
 //the p5js sketch
 const sketch = (p) => {
+
+  // ----------------- create variables
   let positions = {};
   let xx,yy, mouseAngle;
   // xx = 100
@@ -41,9 +44,10 @@ const sketch = (p) => {
     //to fill up the full container, get the width an height
     const containerPos = sketchContainer.getBoundingClientRect();
     //const cnv = p.createCanvas(containerPos.width, containerPos.height); //the canvas!
+
+    // ---------------------- create the canvas as large as the screen width and height
     const cnv = p.createCanvas(window.innerWidth, window.innerHeight);
     //laser = new Laser(width/2, height/2, mouseX, mouseY, 5);
-    p.fill(255); //sets the fill color of the circle to white
     p.frameRate(60); //set framerate to 30, same as server
     socket.on("positions", (data) => {
       //get the data from the server to continually update the positions
@@ -54,24 +58,35 @@ const sketch = (p) => {
   //the p5js draw function, runs every frame rate
   //(30-60 times / sec)
   p.draw = () => {
+
+    // check for wasd / arrow keys to move player
     clientPlayer.move();
+
+    // send new player position to server
     sendPacket();
 
+    // get the target angle that the player has to face
     mouseAngle = p.atan2(p.mouseY - clientPlayer.y - clientPlayer.cameraOffsetY, p.mouseX - clientPlayer.x - clientPlayer.cameraOffsetX)
 
-    //mouseAngle = p.atan2(p.mouseY - window.innerWidth/2, p.mouseX - window.innerHeight/2)
     // laser.move();
     // laser.draw();
     p.background(51); //reset background to black
 
+    // create a grid behind the player
     grid(clientPlayer.cameraOffsetX, clientPlayer.cameraOffsetY)
 
-    //draw a circle for every position
+    // get the angle that the player has to rotate to
     clientPlayer.rotate(mouseAngle);
+
+    // render the player in the middle of the screen
     clientPlayer.render(clientPlayer.cameraOffsetX, clientPlayer.cameraOffsetY);
+
+    // for each client id got from the server except your client, render them
     for (const id in positions) {
       if (id != clientid){
+        // create a player for that id in positions
         const player = new Player(positions[id].name, p, positions[id].x, positions[id].y);
+        // rotate that player and render them
         player.rotate(positions[id].a)
         player.render(clientPlayer.cameraOffsetX, clientPlayer.cameraOffsetY);
       }
@@ -89,7 +104,7 @@ const sketch = (p) => {
     //   player: clientPlayer
     // })
   }
-
+/*
   function render(x, y, targetAngle, name){
     //targetAngle = p.atan2(p.mouseY - y, p.mouseX - x);
 	  currentAngle = lerpAngle(currentAngle, targetAngle, smoothSpeed);
@@ -104,13 +119,8 @@ const sketch = (p) => {
     p.text(name, x, y)
     //console.log(targetAngle)
   }
-
+  */
   function grid(offsetX, offsetY){
-    /*tempoffx = String(offsetX)
-    tempoffy = String(offsetY)
-    offsetX = parseInt(tempoffx.substring(1), 10)
-    offsetY = parseInt(tempoffy.substring(1), 10)*/
-    //console.log(tempoffx, tempoffy)
     for (let i = -100; i < window.innerWidth; i+=100){
       for (let j = -100; j < window.innerHeight; j+=100){
         x=window.innerWidth-i+(offsetX%100)-200
