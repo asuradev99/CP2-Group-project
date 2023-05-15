2125b6230979191284ccc786e75a5fe1892dfc8c
aerits <stev_nm@protonmail.com>
2023-05-15 08:32:50 -0600

    Co-authored-by: Ayush-Ranjan1 <Ayush-Ranjan1@users.noreply.github.com> Co-authored-by: Aditya Suresh <ethanaditya@gmail.com> Co-authored-by: Alongalili0 <Alongalili0@users.noreply.github.com>

--- public/enemy.js
+++ public/enemy.js
@@ -1,31 +1,4 @@
 //ayush
 class Enemy {
-  constructor(x,y,speed,Player) {
-	this.x=x;
-	this.y=y;
-	this.speed=speed;
-	this.Player=Player;
-	this.width=50;
-	this.length=50;
-  }
-  update() {
-    const dx = this.clientplayer.x - this.x;
-    const dy = this.clientplayer.y - this.y;
-    const distance = Math.sqrt(dx*dx+dy*dy);
-  }
-
-  moveTowardsPlayer(){
-  if(distance = 400) {
-    const angle = math.atan2(dy,dx);
-    this.x += this.speed * Math.cos(angle);
-    this.y += this.speed * Math.sin(angle);
-  }
+  constructor(randomx,randomy.,)
 }
-
-  draw() {
-	stroke();
-	rect(x,y,50,50);
-	fill(51);
-  } 
-}
-
---
+++ public/food.js
@@ -0,0 +1,29 @@
+//ayush
+class Food extends entity {
+  constructor(x, y, hp, width){
+    super(x, y, hp, width)
+  }
+
+    render() {
+        stroke(0,0,255);
+        fill(0,0,255);
+        circle(this.x,this.y,10);
+    }
+
+    update(lasers) {
+      for (var j = lasers.length - 1; j>= 0; j--){
+        if(lasers[j].collisionCheck(this) && lasers[j].hit == false){
+            this.hp = this.hp - lasers[j].damage;
+            
+            lasers[j].hit = true;
+        }
+      }
+    }
+
+    killfood(clientPlayer) {
+        if(this.hp<=0){
+          
+            clientPlayer.money += 1;
+        }
+    }
+}
