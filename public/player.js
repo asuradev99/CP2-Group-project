1fba28e3d7e1b5230dbb92e443d06806b06d423a
aerits <stev_nm@protonmail.com>
2023-05-18 17:10:02 -0600

    indentation

--- public/player.js
+++ public/player.js
@@ -1,129 +1,129 @@
 //alon + ayush mostly
 class Player extends entity{
     
-    constructor(playername, x, y, lastShotTime, hp, shield, clientid, points, money, inertia, laserDamage, laserSpeed, isBiden) {
+    constructor(playername, x, y, lastShotTime, hp, shield, clientid, points, money, inertia, laserDamage, laserSpeed, isBiden, turrets) {
 
-      // steven
-      super(x, y, hp);
-      this.width = 30
-      this.playername = playername;
-      this.points = points;
-      this.maxShield = 25;
-      this.shieldRegen = 0.1;
-      this.laserDamage = laserDamage;
-      this.laserSpeed = laserSpeed;
-      this.isHpRegen = false;
-      this.hpRegen = 0;
-      this.maxHp = 100;
-      this.inertia = inertia;
-      this.bulletcounter = 0;
-      this.isBiden = false;
-      this.turrets = [0];
+	// steven
+	super(x, y, hp);
+	this.width = 30
+	this.playername = playername;
+	this.points = points;
+	this.maxShield = 25;
+	this.shieldRegen = 0.1;
+	this.laserDamage = laserDamage;
+	this.laserSpeed = laserSpeed;
+	this.isHpRegen = false;
+	this.hpRegen = 0;
+	this.maxHp = 100;
+	this.inertia = inertia;
+	this.bulletcounter = 0;
+	this.isBiden = false;
+	this.turrets = [0];
 
-      // player stats ayush
-      //this.hp = hp;
-      this.shield = shield;
-      this.movementSpeed = 5;
-      this.reloadTime = 600;
+	// player stats ayush
+	//this.hp = hp;
+	this.shield = shield;
+	this.movementSpeed = 5;
+	this.reloadTime = 600;
 
-      this.sizePenalty = 0;
-      
-      // alon
-      this.money = 8;
+	this.sizePenalty = 0;
+	
+	// alon
+	this.money = 8;
 
-      // player variables ayush + alon
-      this.smoothSpeed = 1;
-      this.targetAngle = 0;
-      this.currentAngle = 0;
-      this.count = 3;
-      this.scl = 25;
-      this.lastShotTime = lastShotTime;
-      this.isShooting = false;
-      this.millisBuffer = 0;
-      this.id = clientid;
+	// player variables ayush + alon
+	this.smoothSpeed = 1;
+	this.targetAngle = 0;
+	this.currentAngle = 0;
+	this.count = 3;
+	this.scl = 25;
+	this.lastShotTime = lastShotTime;
+	this.isShooting = false;
+	this.millisBuffer = 0;
+	this.id = clientid;
 
-      //interpolation stuff (ethan)
-      this.ix = this.x; 
-      this.iy = this.y;
+	//interpolation stuff (ethan)
+	this.ix = this.x; 
+	this.iy = this.y;
     }
 
     removeMoney(i){
-      this.money-=i;
+	this.money-=i;
     }
 
     //ayush
     update(){
-      this.inertia = [0,0];
-      let effectiveSpeed = this.movementSpeed - (this.count - 3);
-      if(keyIsPressed) {
-        if(keyIsDown(LEFT_ARROW)||keyIsDown(65)) {
-          this.x-=effectiveSpeed;
-          this.inertia = [-effectiveSpeed, this.inertia[1]]
-        } 
-        if(keyIsDown(RIGHT_ARROW)||keyIsDown(68)) {
-          this.x+=effectiveSpeed;
-          this.inertia = [+effectiveSpeed, this.inertia[1]]
-        }
-        if(keyIsDown(UP_ARROW)||keyIsDown(87)) {
-          this.y-=effectiveSpeed;
-          this.inertia = [this.inertia[0], -effectiveSpeed]
-        } 
-        if(keyIsDown(DOWN_ARROW)||keyIsDown(83)) {
-          this.y+=effectiveSpeed;
-          this.inertia = [this.inertia[0], +effectiveSpeed]
-        }
-      }
+	this.inertia = [0,0];
+	let effectiveSpeed = this.movementSpeed - (this.count - 3);
+	if(keyIsPressed) {
+            if(keyIsDown(LEFT_ARROW)||keyIsDown(65)) {
+		this.x-=effectiveSpeed;
+		this.inertia = [-effectiveSpeed, this.inertia[1]]
+            } 
+            if(keyIsDown(RIGHT_ARROW)||keyIsDown(68)) {
+		this.x+=effectiveSpeed;
+		this.inertia = [+effectiveSpeed, this.inertia[1]]
+            }
+            if(keyIsDown(UP_ARROW)||keyIsDown(87)) {
+		this.y-=effectiveSpeed;
+		this.inertia = [this.inertia[0], -effectiveSpeed]
+            } 
+            if(keyIsDown(DOWN_ARROW)||keyIsDown(83)) {
+		this.y+=effectiveSpeed;
+		this.inertia = [this.inertia[0], +effectiveSpeed]
+            }
+	}
 
-      // shoot bullet
-      if (mouseIsPressed) {
-        if (performance.now() - this.lastShotTime >= this.reloadTime) {
-          sendBullet(bulletCounter);
-          bulletCounter++;    
-          this.shoot(lasers, performance.now());
-        }
-        this.isShooting = true;
-      } else {
-        this.isShooting = false;
-      }
+	// shoot bullet
+	if (mouseIsPressed) {
+            if (performance.now() - this.lastShotTime >= this.reloadTime) {
+		sendBullet(bulletCounter);
+		bulletCounter++;    
+		this.shoot(lasers, performance.now());
+            }
+            this.isShooting = true;
+	} else {
+            this.isShooting = false;
+	}
     }
     //alon
     shoot(lasers, millis) {
-      if (millis - this.lastShotTime >= this.reloadTime) {
-        for(let i = 0; i<this.turrets.length; i++){
-          let laser = new Laser(this.x, this.y, this.currentAngle+this.turrets[i], this.laserSpeed - (this.count - 3), 500, this, this.id, this.laserDamage, this.inertia, bulletCounter, this.width / 2);
-          lasers.push(laser);
-          
-          this.bulletcounter++;
-        }
-        this.lastShotTime = millis;
-      }
+	if (millis - this.lastShotTime >= this.reloadTime) {
+            for(let i = 0; i<this.turrets.length; i++){
+		let laser = new Laser(this.x, this.y, this.currentAngle+this.turrets[i], this.laserSpeed - (this.count - 3), 500, this, this.id, this.laserDamage, this.inertia, bulletCounter, this.width / 2);
+		lasers.push(laser);
+		
+		this.bulletcounter++;
+            }
+            this.lastShotTime = millis;
+	}
 
     }
     //alon + ayush
     lerpAngle(a, b, step) {
-      // Prefer shortest distance,
-      const delta = b - a;
-      if (delta == 0.0) {
-          return a;
-      } else if (delta < -PI) {
-          b += TWO_PI;
-      } else if (delta > PI) {
-          a += TWO_PI;
-      }
-      return (1.0 - step) * a + step * b;
+	// Prefer shortest distance,
+	const delta = b - a;
+	if (delta == 0.0) {
+            return a;
+	} else if (delta < -PI) {
+            b += TWO_PI;
+	} else if (delta > PI) {
+            a += TWO_PI;
+	}
+	return (1.0 - step) * a + step * b;
     }
-      
+    
     rotate(targetAngle){
         this.currentAngle = this.lerpAngle(this.currentAngle, targetAngle, this.smoothSpeed);
     }
     reward(pts) {
-      this.points += pts
-      this.count = 3 + Math.floor(this.points / 300)
-      this.width = 30 + (this.count - 3) * 10;
-      //size penalty
+	this.points += pts
+	this.count = 3 + Math.floor(this.points / 300)
+	this.width = 30 + (this.count - 3) * 10;
+	//size penalty
     }
     render(laser){
-      beginShape();
+	beginShape();
         for (let i = 0; i < this.count; ++i) {
             const theta = this.currentAngle + i * TWO_PI / this.count;
             vertex( (this.x) + cos(theta) * this.width, (this.y) + sin(theta) * this.width);
@@ -139,12 +139,12 @@ class Player extends entity{
         // steven
         if(!(this == this)) {
 
-        let pointtext = this.points
-        text(pointtext, this.x - 10 , this.y+60);
+            let pointtext = this.points
+            text(pointtext, this.x - 10 , this.y+60);
         }
 
-      // hitbox
-      circle(this.x, this.y, this.width);
+	// hitbox
+	circle(this.x, this.y, this.width);
 	
         //text(this.hp, this.x, this.y+20);
 
@@ -167,27 +167,27 @@ class Player extends entity{
         fill(31,190,214);
         rect(this.x+xoffset,this.y+10+this.width,this.shield*(100/this.maxShield),16);
 
-  //       //money bar ayush
-  //       stroke(51,0,0);
-  //       strokeWeight(4);
-  //       noFill()
-  //       rect(this.x-this.width*2-10,this.y+60,100,20);
-  //       noStroke();
+	//       //money bar ayush
+	//       stroke(51,0,0);
+	//       strokeWeight(4);
+	//       noFill()
+	//       rect(this.x-this.width*2-10,this.y+60,100,20);
+	//       noStroke();
 
 	// // steven
-  //       if(this.money<101){
-  //         fill(255,215,0);
-  //         rect(this.x-this.width*2-10,this.y+60,this.money,16);
-  //       } else{
-  //         fill(255-(this.money-100)/2,215,0);
-  //         rect(this.x-this.width*2-10,this.y+60,100,16);
-  //         fill(255-this.money/2,100,0);
-  //         rect(this.x-this.width*2-10,this.y+60,this.money%100,16);
-  //       }
+	//       if(this.money<101){
+	//         fill(255,215,0);
+	//         rect(this.x-this.width*2-10,this.y+60,this.money,16);
+	//       } else{
+	//         fill(255-(this.money-100)/2,215,0);
+	//         rect(this.x-this.width*2-10,this.y+60,100,16);
+	//         fill(255-this.money/2,100,0);
+	//         rect(this.x-this.width*2-10,this.y+60,this.money%100,16);
+	//       }
 
 	// fill(0,0,0)
-  //       stroke(255,0,0)
-  //       strokeWeight(3)
+	//       stroke(255,0,0)
+	//       strokeWeight(3)
 	
 	
 
@@ -200,18 +200,18 @@ class Player extends entity{
     }
 
     updateShield() {
-      //shield come back
-      // steven
-      if (performance.now() - lastHitTime > 5000) {
-        if (this.shield < this.maxShield) {
-          this.shield = this.shield + this.shieldRegen;
-        }
-        if (this.isHpRegen) {
-          if (this.hp < this.maxHp) {
-            this.hp = this.hp + this.hpRegen;
-          }
-        }
-      }
+	//shield come back
+	// steven
+	if (performance.now() - lastHitTime > 5000) {
+            if (this.shield < this.maxShield) {
+		this.shield = this.shield + this.shieldRegen;
+            }
+            if (this.isHpRegen) {
+		if (this.hp < this.maxHp) {
+		    this.hp = this.hp + this.hpRegen;
+		}
+            }
+	}
     }
 }
 
